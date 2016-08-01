6872d6e83b3361ac601d18b142b6138778709702
Demitri Swan <demitri@moz.com>
2016-07-31 22:20:28 -0700

    Adding tests for VagrantSpec::Utils

--- lib/vagrant_spec/utils.rb
+++ lib/vagrant_spec/utils.rb
@@ -5,17 +5,19 @@ module VagrantSpec
   module Utils
     # return [String]
     def project_root
-      File.absolute_path(File.join(File.dirname(__FILE__), '..', '..'))
+      File.expand_path(File.join(File.dirname(__FILE__), '..', '..'), __FILE__)
     end
 
     # return [String]
     def lib_root
-      File.absolute_path(File.join(File.dirname(__FILE__), '..'))
+      File.expand_path(File.join(File.dirname(__FILE__), '..'), __FILE__)
     end
 
     # return [String]
     def template_dir
-      File.absolute_path(File.join(lib_root, 'vagrant_spec', 'templates'))
+      File.expand_path(
+        File.join(lib_root, 'vagrant_spec', 'templates'), __FILE__
+      )
     end
   end
 end
---
+++ spec/unit/vagrant_spec_test/utils_test.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+require 'vagrant_spec/utils'
+
+describe VagrantSpec::Utils do
+  let(:klass) do 
+    class Klass
+      include VagrantSpec::Utils
+    end
+    Klass.new
+  end
+
+  it '#project_root dirname is vagrant_spec' do
+    expect(File.basename(klass.project_root)).to eq('vagrant_spec')
+  end
+
+  it '#lib_root contains vagrant_spec/lib' do
+    expect(klass.lib_root).to match(/.*vagrant_spec\/lib/)
+  end
+
+  it '#template_dir contains vagrant_spec/lib/vagrant_spec/templates' do
+    expect(klass.template_dir).to match(
+      /.*vagrant_spec\/lib\/vagrant_spec\/templates/
+    )
+  end
+end
